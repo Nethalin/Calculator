6038262c39da9df159798921a14189edb7865106
Your Name <administrator@CN-7.lan>
2019-06-19 17:41:21 +0100

    Updates

---
+++ precache-manifest.10857bfd64c48101c9ae4e0c7367d90a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa48c84a25385f4a525584541e0bd26",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "84b3e582f3ace55d6759",
+    "url": "/Calculator/static/css/main.608cff0a.chunk.css"
+  },
+  {
+    "revision": "1fdb998c78d888c154de",
+    "url": "/Calculator/static/js/2.fdfc7398.chunk.js"
+  },
+  {
+    "revision": "84b3e582f3ace55d6759",
+    "url": "/Calculator/static/js/main.d7f9a8c5.chunk.js"
+  },
+  {
+    "revision": "cf49d768c4d3a5b9fc25",
+    "url": "/Calculator/static/js/runtime~main.8078b280.js"
+  }
+]);
